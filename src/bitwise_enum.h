5f5a140218601be8948d07f6db3e3ebb15fdad52
Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>
2025-05-08 15:27:28 +0000

    Start implementing proper argument parser

---
+++ src/bitwise_enum.h
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+
+#ifndef PACMANPP_SRC_BITWISE_ENUM_H_
+#define PACMANPP_SRC_BITWISE_ENUM_H_
+
+#include <type_traits>
+#include <utility>
+
+namespace pacmanpp::util {
+
+template <typename Enum>
+struct EnableEnumBitwiseOperators {
+  static constexpr bool enabled = false;
+};
+
+template <typename Enum>
+concept BitwiseEnum =
+    std::is_enum_v<Enum> && EnableEnumBitwiseOperators<Enum>::enabled;
+
+template <typename Enum>
+  requires BitwiseEnum<Enum>
+constexpr Enum operator|(const Enum lhs, const Enum rhs) {
+  return static_cast<Enum>(std::to_underlying(lhs) | std::to_underlying(rhs));
+}
+
+template <typename Enum>
+  requires BitwiseEnum<Enum>
+constexpr Enum operator&(const Enum lhs, const Enum rhs) {
+  return static_cast<Enum>(std::to_underlying(lhs) & std::to_underlying(rhs));
+}
+
+template <typename Enum>
+  requires BitwiseEnum<Enum>
+constexpr Enum operator^(const Enum lhs, const Enum rhs) {
+  return static_cast<Enum>(std::to_underlying(lhs) ^ std::to_underlying(rhs));
+}
+
+template <typename Enum>
+  requires BitwiseEnum<Enum>
+constexpr Enum operator~(const Enum lhs) {
+  return static_cast<Enum>(~std::to_underlying(lhs));
+}
+
+template <typename Enum>
+  requires BitwiseEnum<Enum>
+Enum &operator|=(Enum &lhs, const Enum rhs) {
+  lhs = static_cast<Enum>(std::to_underlying(lhs) | std::to_underlying(rhs));
+  return lhs;
+}
+
+template <typename Enum>
+  requires BitwiseEnum<Enum>
+Enum &operator&=(Enum &lhs, const Enum rhs) {
+  lhs = static_cast<Enum>(std::to_underlying(lhs) & std::to_underlying(rhs));
+  return lhs;
+}
+
+template <typename Enum>
+  requires BitwiseEnum<Enum>
+Enum &operator^=(Enum &lhs, const Enum rhs) {
+  lhs = static_cast<Enum>(std::to_underlying(lhs) ^ std::to_underlying(rhs));
+  return lhs;
+}
+
+}  // namespace pacmanpp::util
+
+#endif  // PACMANPP_SRC_BITWISE_ENUM_H_
