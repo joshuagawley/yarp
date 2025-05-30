6038c6d1881cc53e7dd72877f99f5e3b11ad6cc0
Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>
2025-05-30 16:38:35 +0100

    Remove commented out code in args001.py

--- tests/args001.py
+++ tests/args001.py
@@ -1,22 +1,5 @@
 # SPDX-License-Identifier: MIT
 
-# import subprocess
-# import sys
-
-# pacmanpp = sys.argv[1]
-
-# expected = "Error: no operation specified (use -h for help)"
-# output = subprocess.run([pacmanpp], capture_output=True, text=True).stdout.strip()
-
-# if output != expected:
-#     print(f"Expected: {expected}")
-#     print(f"Output: {output}")
-#     return_code = 1
-# else:
-#     return_code = 0
-
-# sys.exit(return_code)
-
 import pptest
 import sys
 
