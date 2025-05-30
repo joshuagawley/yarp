d51f12cd17224da9c3714883cad7b3caa7b29de7
Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>
2025-05-30 17:43:44 +0100

    Combine custom root path and database path tests

--- tests/args004.py
+++ tests/args004.py
@@ -6,7 +6,7 @@ import sys
 test = pptest.Test(sys.argv[1])
 
 with pptest.TestEnvironment() as env:
-    result = test.run(["--root", str(env.root), "-Q"])
+    result = test.run(["--root", str(env.root), "--dbpath", str(env.db_path), "-Q"])
     test.assert_returncode(result, 0)
     test.assert_equals(result.stdout, "")
 
