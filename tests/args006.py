0abc9070ff49d027e80d595c3091ff90ec0d6e89
Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>
2025-05-30 16:51:19 +0100

    Refactor args006.py

--- tests/args006.py
+++ tests/args006.py
@@ -1,20 +1,8 @@
 # SPDX-License-Identifier: MIT
 
-import subprocess
+import pptest
 import sys
 
-pacmanpp = sys.argv[1]
-
-expected = "Error: package firefox not found\n"
-output = subprocess.run(
-    [pacmanpp, "-Q", "firefox"], capture_output=True, text=True
-).stdout
-
-if output != expected:
-    print(f"Expected: {expected}")
-    print(f"Output: {output}")
-    return_code = 1
-else:
-    return_code = 0
-
-sys.exit(return_code)
+test = pptest.Test(sys.argv[1])
+test.test_exact_output(["-Q", "firefox"], "Error: package firefox not found\n")
+test.exit_with_result()
