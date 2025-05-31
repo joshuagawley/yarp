1e73b6cd96d19841ef4afc13f7d08751b5a5f4b8
Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>
2025-05-31 12:10:31 +0100

    Add test for long query flag

---
+++ tests/query004.py
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: MIT
+
+import pptest
+import sys
+
+test = pptest.Test(sys.argv[1])
+test.test_exact_output(["--query", "pacman"], "pacman 7.0.0.r6.gc685ae6-2\n")
+test.exit_with_result()
