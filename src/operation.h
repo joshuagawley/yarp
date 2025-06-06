1af03bf3da500e424f2870dcdd333a2f34120f6f
Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>
2025-06-06 10:16:30 +0100

    Add QueryOptions bitfield

--- src/operation.h
+++ src/operation.h
@@ -3,6 +3,8 @@
 #ifndef PACMANPP_SRC_OPERATION_H_
 #define PACMANPP_SRC_OPERATION_H_
 
+#include <cstdint>
+
 #include "src/bitwise_enum.h"
 
 namespace pacmanpp {
@@ -15,6 +17,26 @@ enum class Operation {
 
 template <>
 struct util::EnableEnumBitwiseOperators<Operation> {
+  static constexpr bool enabled = true;
+};
+
+enum class QueryOptions : std::uint16_t {
+  kIsFile = 1 << 0,
+  kInfo = 1 << 1,
+  kList = 1 << 2,
+  kUnrequired = 1 << 3,
+  kDeps = 1 << 4,
+  kExplicit = 1 << 5,
+  kOwns = 1 << 6,
+  kSearch = 1 << 7,
+  kChangelog = 1 << 8,
+  kUpgrade = 1 << 9,
+  kCheck = 1 << 10,
+  kLocality = 1 << 11,
+};
+
+template <>
+struct util::EnableEnumBitwiseOperators<QueryOptions> {
   static constexpr bool enabled = true;
 };
 
