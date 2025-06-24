32bbc3f6cf24984208662ee6fa1df590d68ff067
Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>
2025-06-24 09:22:53 +0100

    Use real package for test

--- tests/query004.py
+++ tests/query004.py
@@ -6,23 +6,18 @@ import sys
 test = pptest.Test(sys.argv[1])
 
 with pptest.TestEnvironment() as env:
-    pkg = env.create_package("foo", "1.0.0", "bar")
-    pkg.files = ["bin/foobar"]
-    pkg.groups = ["bar"]
-    pkg.url = "https://foobar.com/foo"
-    pkg.license = "MIT"
-    pkg.architecture = "x86_64"
-    pkg.packager = "Joshua Gawley"
 
     result = test.run(
-        ["--root", str(env.root), "--dbpath", str(env.db_path), "-Qi", "foo"]
+        ["-Qi", "powertop"]
     )
 
     test.assert_returncode(result, 0)
-    test.assert_contains(result.stdout, "Name            : foo\n")
-    test.assert_contains(result.stdout, "Version         : 1.0.0\n")
-    test.assert_contains(result.stdout, "Description     : bar\n")
+    test.assert_contains(result.stdout, "Name            : powertop\n")
+    test.assert_contains(result.stdout, "Version         : 2.15-2\n")
+    test.assert_contains(result.stdout, "Description     : A tool to diagnose issues with power consumption and power management\n")
     test.assert_contains(result.stdout, "Architecture    : x86_64\n")
     # test.assert_contains(result.stdout, "URL             : https://foobar.com/foo\n")
     # test.assert_contains(result.stdout, "License         : MIT\n")
     # test.assert_contains(result.stdout, "Packager        : Joshua Gawley\n")
+
+test.exit_with_result()
