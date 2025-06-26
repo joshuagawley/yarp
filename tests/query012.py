1a36863903f7fbc5af1d1966ac83c49290fd17e9
Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>
2025-06-26 19:21:07 +0100

    Use powertop instead of pacman in tests/query012.py

--- tests/query012.py
+++ tests/query012.py
@@ -8,10 +8,10 @@ test = pptest.Test(sys.argv[1])
 with pptest.TestEnvironment() as env:
 
     result = test.run(
-        ["-Qk", "pacman"]
+        ["-Qk", "powertop"]
     )
 
     test.assert_returncode(result, 0)
-    test.assert_contains(result.stdout, "pacman: 422 total files, 0 missing files")
+    test.assert_contains(result.stdout, "powertop: 41 total files, 0 missing files")
 
 test.exit_with_result()
