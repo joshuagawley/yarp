dce257a570b9d46a3372c9951e1979970cb84d2f
Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>
2025-06-26 13:49:45 +0100

    Create tests for querying group information

---
+++ tests/query007.py
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: MIT
+
+import pptest
+import sys
+
+test = pptest.Test(sys.argv[1])
+
+with pptest.TestEnvironment() as env:
+    
+    result = test.run(
+        ["-Qg", "ttf-nerd-fonts-symbols"]
+    )
+
+    test.assert_returncode(result, 0)
+    test.assert_contains(result.stdout, "nerd-fonts ttf-nerd-fonts-symbols\n")
