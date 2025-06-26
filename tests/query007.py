af6cdd490bf91d2ca726f7b08b5d10f3f2d5522f
Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>
2025-06-26 13:51:53 +0100

    Make group query tests actually run

--- tests/query007.py
+++ tests/query007.py
@@ -6,10 +6,12 @@ import sys
 test = pptest.Test(sys.argv[1])
 
 with pptest.TestEnvironment() as env:
-    
+
     result = test.run(
         ["-Qg", "ttf-nerd-fonts-symbols"]
     )
 
     test.assert_returncode(result, 0)
     test.assert_contains(result.stdout, "nerd-fonts ttf-nerd-fonts-symbols\n")
+
+test.exit_with_result()
