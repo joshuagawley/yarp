acd0a742a016858665301650a8db6de7ce427e0d
Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>
2025-06-26 16:59:42 +0100

    Change package tested in tests/query009.py

--- tests/query009.py
+++ tests/query009.py
@@ -8,10 +8,10 @@ test = pptest.Test(sys.argv[1])
 with pptest.TestEnvironment() as env:
 
     result = test.run(
-        ["-Qd", "gcc-libs"]
+        ["-Qd", "jsoncpp"]
     )
 
     test.assert_returncode(result, 0)
-    test.assert_contains(result.stdout, "gcc-libs 15.1.1+r7+gf36ec88aa85a-1")
+    test.assert_contains(result.stdout, "jsoncpp 1.9.6-3")
 
 test.exit_with_result()
