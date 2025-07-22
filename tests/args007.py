386eb14f2087aef4ab32a8620a11cd5154344411
Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>
2025-07-22 22:14:57 +0100

    Fix broken test for testing passing in config file

--- tests/args007.py
+++ tests/args007.py
@@ -4,8 +4,8 @@ import pptest
 import sys
 
 test = pptest.Test(sys.argv[1])
-result = test.run(["--config", "test_pacman_conf.py"])
+result = test.run(["--verbose", "--config", "test_pacman_conf.conf"])
 
-test.assert_returncode(result, 0)
-test.assert_contains(result.stdout, "Conf File : test_pacman_conf.conf\n")
+test.assert_returncode(result, 1)
+test.assert_contains(result.stdout, "Conf File  : test_pacman_conf.conf\n")
 test.exit_with_result()
