6ef0e6e83857e85fbd036578f12367a3689a88b6
Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>
2025-07-23 10:57:20 +0100

    Rewrite query001.py to use mock database (proof of concept)

--- tests/query001.py
+++ tests/query001.py
@@ -2,15 +2,25 @@
 
 import pptest
 import sys
+from pathlib import Path
 
 test = pptest.Test(sys.argv[1])
 
-with pptest.TestEnvironment() as env:
-    pkg = env.create_package("foo", "1.0.0", "foo")
+# Get the path to the test-data directory in the build directory
+build_dir = Path(sys.argv[1]).parent
+test_data_dir = build_dir / "tests" / "test-data"
+config_path = test_data_dir / "pacman.conf"
+db_path = test_data_dir / "db"
 
-    result = test.run(
-        ["--root", str(env.root), "--dbpath", str(env.db_path), "-Q", "foo"]
-    )
+# Run the query using the mock database
+result = test.run([
+    "--root", "/var/empty",
+    # "--config", str(config_path)
+    "--dbpath", str(db_path),
+    "-Q", "pacman"
+])
 
-    test.assert_returncode(result, 0)
-    test.assert_equals(result.stdout, "foo 1.0.0\n")
+# Check the result
+test.assert_returncode(result, 0)
+test.assert_equals(result.stdout, "pacman 5.2.2-3\n")
+test.exit_with_result()
