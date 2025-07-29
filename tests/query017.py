2833e5fedb92e22f66f2744e9e0698d36a1c4ec9
Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>
2025-07-29 15:12:04 +0100

    Correct -Qu test

--- tests/query017.py
+++ tests/query017.py
@@ -10,6 +10,6 @@ test.run_pacman(["-Sy"])
 result = test.run(["-Qu", "cmake"])
 
 test.assert_returncode(result, 0)
-test.assert_contains(result.stdout, "cmake 3.20.0-1 -> 4.0.3-1")
+test.assert_contains(result.stdout, "cmake 3.20.0-1 -> 3.20.2-1")
 
 test.exit_with_result()
