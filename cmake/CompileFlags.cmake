9b6e314188b0ea35981283065db9f30b5e3f1c71
Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>
2025-08-19 17:17:21 +0100

    Extract various setup processes into separate CMake files

---
+++ cmake/CompileFlags.cmake
@@ -0,0 +1,60 @@
+function(
+        pacmanpp_set_project_compile_flags
+        common_target_name
+        WARNING_AS_ERRORS
+)
+    set(COMPILE_FLAGS
+            -Wall
+            -Wextra # reasonable and standard
+            -Wshadow # warn the user if a variable declaration shadows one from a parent context
+            -Wnon-virtual-dtor # warn the user if a class with virtual functions has a non-virtual destructor. This helps
+            # catch hard to track down memory errors
+            -Wold-style-cast # warn for c-style casts
+            -Wcast-align # warn for potential performance problem casts
+            -Wunused # warn on anything being unused
+            -Woverloaded-virtual # warn if you overload (not override) a virtual function
+            -Wpedantic # warn if non-standard C++ is used
+            -Wconversion # warn on type conversions that may lose data
+            -Wsign-conversion # warn on sign conversions
+            -Wnull-dereference # warn if a null dereference is detected
+            -Wdouble-promotion # warn if float is implicit promoted to double
+            -Wformat=2 # warn on security issues around functions that format output (ie printf)
+            -Wimplicit-fallthrough # warn on statements that fallthrough without an explicit annotation
+    )
+
+    if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        list(APPEND
+                COMPILE_FLAGS
+                -Wmisleading-indentation # warn if indentation implies blocks where blocks do not exist
+                -Wduplicated-cond # warn if if / else chain has duplicated conditions
+                -Wduplicated-branches # warn if if / else branches have duplicated code
+                -Wlogical-op # warn about logical operations being used where bitwise were probably wanted
+                -Wuseless-cast # warn if you perform a cast to the same type
+                -Wsuggest-override # warn if an overridden member function is not marked 'override' or 'final'
+        )
+    endif ()
+
+    if (WARNINGS_AS_ERRORS)
+        message(TRACE "Warnings are treated as errors")
+        list(APPEND COMPILE_FLAGS -Werror)
+    endif ()
+
+    if (CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+        set(PROJECT_WARNINGS_CXX ${CLANG_WARNINGS})
+    elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        set(PROJECT_WARNINGS_CXX ${GCC_WARNINGS})
+    endif ()
+
+    # use the same warning flags for C
+    set(PROJECT_WARNINGS_C "${PROJECT_WARNINGS_CXX}")
+
+    target_compile_options(
+            ${common_target_name}
+            INTERFACE # C++ warnings
+            $<$<COMPILE_LANGUAGE:CXX>:${PROJECT_WARNINGS_CXX}>
+            # C warnings
+            $<$<COMPILE_LANGUAGE:C>:${PROJECT_WARNINGS_C}>
+            # Cuda warnings
+            $<$<COMPILE_LANGUAGE:CUDA>:${PROJECT_WARNINGS_CUDA}>)
+
+endfunction()
---
+++ cmake/TestUtils.cmake
@@ -0,0 +1,56 @@
+function(pacmanpp_add_test)
+    cmake_parse_arguments(ARG "" "NAME;DESCRIPTION" "" ${ARGN})
+
+    if (NOT ARG_NAME)
+        message(FATAL_ERROR "Could not find NAME")
+    endif ()
+
+    if (NOT ARG_DESCRIPTION)
+        message(FATAL_ERROR "Could not find NAME")
+    endif ()
+
+    add_test(NAME "${ARG_DESCRIPTION}"
+            COMMAND ${Python3_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_NAME}.py ${CMAKE_BINARY_DIR}/pacmanpp
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    )
+endfunction(pacmanpp_add_test)
+
+function(pacmanpp_add_unit_test)
+    cmake_parse_arguments(ARG "" "NAME" "SOURCES;COPY_FILES;LIBRARIES" ${ARGN})
+
+    if (NOT ARG_NAME)
+        message(FATAL_ERROR "Could not find NAME for C++ test")
+    endif ()
+
+    if (NOT ARG_SOURCES)
+        message(FATAL_ERROR "Could not find SOURCES for C++ test")
+    endif ()
+
+    add_executable(${ARG_NAME} ${ARG_SOURCES})
+
+    target_sources(${ARG_NAME}
+            PRIVATE
+            ${ARG_SOURCES}
+    )
+
+    target_include_directories(${ARG_NAME} PRIVATE
+            ${CMAKE_SOURCE_DIR}/src
+            ${CMAKE_BINARY_DIR}/src
+    )
+
+    # Link against Catch2 and any additional libraries in a single call
+    target_link_libraries(${ARG_NAME} PRIVATE project_settings Catch2::Catch2WithMain ${ARG_LIBRARIES})
+
+    # Copy any specified files to the build directory
+    if (ARG_COPY_FILES)
+        foreach (file ${ARG_COPY_FILES})
+            add_custom_command(TARGET ${ARG_NAME} POST_BUILD
+                    COMMAND ${CMAKE_COMMAND} -E copy
+                    ${CMAKE_CURRENT_SOURCE_DIR}/${file}
+                    ${CMAKE_CURRENT_BINARY_DIR}/${file}
+            )
+        endforeach ()
+    endif ()
+
+    catch_discover_tests(${ARG_NAME})
+endfunction(pacmanpp_add_unit_test)
