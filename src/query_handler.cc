69c8201d76de619fb6d8d651f59afcec4e1b88b3
Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>
2025-09-08 11:00:50 +0100

    Remove dead code from query_handler.cc

--- src/query_handler.cc
+++ src/query_handler.cc
@@ -368,37 +368,4 @@ bool QueryHandler::FilterPkg(const alpmpp::AlpmPackage &pkg) const {
          (only_upgrade && !IsUpgradable(pkg));
 }
 
-// std::expected<void, std::string> QueryHandler::PrintPkgSearch() const {
-//   const std::vector<alpmpp::AlpmPackage> search_list =
-//       alpmpp::Alpm::DbSearch(local_db_, targets_);
-//
-//   if (search_list.empty()) {
-//     return std::unexpected("Error: could not determine search list");
-//   }
-//
-//   std::stringstream ss;
-//
-//   for (const alpmpp::AlpmPackage &pkg : search_list) {
-//     const std::vector<std::string_view> groups = pkg.groups();
-//
-//     std::print(ss, "{}/{} {}", alpm_db_get_name(local_db_), pkg.name(),
-//                pkg.version());
-//     if (!groups.empty()) {
-//       std::print(ss, " (");
-//       for (const std::string_view &group : groups) {
-//         if (group != *(std::end(groups) - 1)) {
-//           std::print(ss, "{}  ", group);  // Not the last item, add space
-//         } else {
-//           std::print(ss, "{}", group);  // Last item, no trailing space
-//         }
-//       }
-//       std::print(ss, ")");
-//     }
-//     std::println(ss, "\n    {}", pkg.desc());
-//   }
-//   std::println("{}", ss.str());
-//
-//   return {};
-// }
-
 }  // namespace yarp
